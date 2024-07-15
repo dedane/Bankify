ac31def2544b1258b11069266fff54e5ade7c93c
dedane <evanskeema@gmail.com>
2024-07-15 22:52:00 +0300

    right-side-bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,27 @@
+import Image from "next/image";
+import SideBar from "../../components/ui/SideBar";
+import MobileNavBar from "../../components/ui/MobileNavBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Evans', lastName: 'Kimathi'}
   return (
     <main className="flex h-screen w-full font-inter">
-        SIDEBAR
-        <div className="flex size-full flex-col">
-        {children}
-          
-            
-        
+       <SideBar user={loggedIn}/>
+       <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src='/icons/logo.svg' width={30} height={30} alt="logo"/>
+          <div>
+            <MobileNavBar user={loggedIn} />
+          </div>
         </div>
-        
+        {children}
+       </div>
+   
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from '../../components/HeaderBox'
 import TotalBalanceBox from '../../components/TotalBalanceBox';
+import RightSideBar from '../../components/ui/RightSideBar';
 
 function Home() {
-    const loggedIn = {firstName: 'Evans'};
+    const loggedIn = {firstName: 'Evans', lastName: 'Kimathi', email: 'evanskeema@gmail.com'};
   return (
     <section className='home'>
         <div className='home-content'>
@@ -20,7 +21,12 @@ function Home() {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+            RECENT TRANSACTIONS
         </div>
+        <RightSideBar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 123.50}, {currentBalance: 203.50}]} />
     </section>
   )
 }
